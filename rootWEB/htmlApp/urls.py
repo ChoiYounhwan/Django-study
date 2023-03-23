33b6b995a5b69bad16e604e3a3016b9c11a62b68
Choiyounhwan <cyhwan0816@gmail.com>
2023-03-23 15:24:08 +0900

    commit2

--- rootWEB/htmlApp/urls.py
+++ rootWEB/htmlApp/urls.py
@@ -4,7 +4,12 @@ from htmlApp import views
 
 # http://127.0.0.1:8000/html/xxxx
 urlpatterns = [
-     # http://127.0.0.1:8000/html/xxxx
+     # http://127.0.0.1:8000/html/index
     path('index/', views.index),
-    path('basic_tag/', views.tag)
+    # http://127.0.0.1:8000/html/basic_tag
+    path('basic_tag/', views.tag),
+    # http://127.0.0.1:8000/html/form_tag
+    path('form_tag/' , views.form),
+    # http://127.0.0.1:8000/html/login
+    path('login/'    , views.login)
 ]
--- rootWEB/htmlApp/views.py
+++ rootWEB/htmlApp/views.py
@@ -15,3 +15,22 @@ def tag(request) :
     print('>>>>>> debug, client url http://127.0.0.1:8000/html/index/basic_tag , tag() call ~~ ')
 
     return render(request, 'html/tag.html')
+
+def form(request) :
+    print('>>>>>> debug, client url http://127.0.0.1:8000/html/index/form_tag , form() call ~~ ')
+    return render(request, 'html/form.html')
+
+def login(request) :
+    print('>>>>>> debug, client url http://127.0.0.1:8000/html/index/login , login() call ~~ ')
+    # 1. 사용자의 아이디, 패스워드를 얻어와야한다.
+    # 2. model - select
+    # 3. 사용자 인증에 따른 화면분기(ok, fail)
+    id = request.GET['user_id']
+    pwd = request.GET['user_pwd']
+    print('>>>>>> debug , params : ', id, pwd)
+    if id == 'jslim' and pwd == 'jslim':
+        context = { 'nickName' : '나가사키'}
+        return render(request, 'html/ok.html' , context)
+    else :
+        context = {'id': id}
+        return render(request, 'html/fail.html' , context)
